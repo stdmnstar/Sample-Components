806e986884cbb5fd9558a5199142171df8d1f7c2
Aliaksandr Tarhonski <stdmnstar@gmail.com>
2020-10-20 04:22:33 +0300

    feat: smooth image change

---
+++ Smooth image change/script.js
@@ -0,0 +1,22 @@
+//const base = 'https://raw.githubusercontent.com/irinainina/ready-projects/momentum/momentum/assets/images/night/';
+const base = './assets/images/night/';
+const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+let i = 0;
+
+function viewBgImage(data) {
+  const body = document.querySelector('body');
+  const src = data;
+  const img = document.createElement('img');
+  img.src = src;
+  img.onload = () => {      
+    body.style.backgroundImage = `url(${src})`;
+  }; 
+}
+function getImage() {
+  const index = i % images.length;
+  const imageSrc = base + images[index];
+  viewBgImage(imageSrc);
+  i++;
+} 
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', getImage);
